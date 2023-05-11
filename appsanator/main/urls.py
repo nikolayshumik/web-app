e011fd0a9ab338cff2d7680eafdee34d8304f273
Nikolay <nikolayshumik@gmail.com>
2023-05-11 13:58:43 +0300

    Добавлены переходы по кнопкам

--- appsanator/main/urls.py
+++ appsanator/main/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
     path('create', views.create, name='create'),
+    path('afisha', views.afisha, name='afisha'),
+    path('book', views.book, name='book'),
+    path('shema', views.shema, name='shema'),
 ]
--- appsanator/main/views.py
+++ appsanator/main/views.py
@@ -7,8 +7,21 @@ def index(request):
     return render(request, 'main/index.html', {'news': news})
 
 def about(request):
-    return render(request, 'main/about.html')
+    news = Raspisanie.objects.all()
+    return render(request, 'main/about.html', {'news': news})
 
 def create(request):
-    return render(request, 'main/create.html')
+    news = Raspisanie.objects.all()
+    return render(request, 'main/create.html', {'news': news})
 
+def afisha(request):
+    news = Raspisanie.objects.all()
+    return render(request, 'main/afisha.html', {'news': news})
+
+def book(request):
+    news = Raspisanie.objects.all()
+    return render(request, 'main/book.html', {'news': news})
+
+def shema(request):
+    news = Raspisanie.objects.all()
+    return render(request, 'main/shema.html', {'news': news})
